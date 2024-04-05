c7398460cf10fcd61d878f9e97d4a1465b4271da
Eduardo <cilento762@gmail.com>
2024-04-05 16:22:40 -0300

    First class Position pt2

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -31,11 +31,6 @@ public class Position {
 		this.column = column;
 	}
 	
-	
-	public void setValues(int row, int column) {
-		
-	}
-	
 	@Override 
 	public String toString() {
 		return row + ", " + column;
